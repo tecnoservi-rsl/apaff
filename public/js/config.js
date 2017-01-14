2871e681271da6b1d0388596e0585082e6a11469
angel charlot <legna18205@gmail.com>
2017-01-14 09:07:59 -0400

    ultima correcion de ortografia

--- public/js/config.js
+++ public/js/config.js
@@ -1,6 +1,6 @@
 //var base_url = 'http://americas89.no-ip.info:8080/didactico/';
-//var base_url = 'http://192.168.0.5/didactico/';
-var base_url = 'http://www.fudafamilia.esy.es/';
+var base_url = 'http://localhost/apaff/';
+//var base_url = 'http://www.fudafamilia.esy.es/';
 
 
 
